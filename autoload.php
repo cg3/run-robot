393f400d7a11cbcab8f8b24c00c4a51cb32e55a1
Christian Götz <github@mohk.de>
2013-03-03 14:26:38 +0100

    PathArray um zusaetzliche Ordner erweitert

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 function __autoload($class_name) 
 {
-    $applicationPathList = array('.', 'html', 'model');
+    $applicationPathList = array('.', 'html', 'model', 'Model', 'Loader', 'Solver', 'Responder');
     foreach($applicationPathList as $path)
     {
         $classFile = $path . '/' .$class_name. '.php';
